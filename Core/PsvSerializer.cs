d3a9476d5679e9b67089fd88cdfc2ed05e8f13ac
Starinin Andrey <starininandrey@gmail.com>
2024-01-10 20:45:56 +0300

    Update PsvSerializer (add PsvNameAttribute)

--- Core/PsvSerializer.cs
+++ Core/PsvSerializer.cs
@@ -6,25 +6,63 @@ namespace PSVSerializer.Core;
 
 public static class PsvSerializer
 {
-    public static string Serialize(object obj)
+    private static readonly StringBuilder StringBuilder = new();
+
+    private static void RemoveLastDelimiter()
     {
-        var stringBuilder = new StringBuilder();
+        StringBuilder.Remove(StringBuilder.Length - 1, 1);
+    }
+    private static string GetPropertyNames(IEnumerable<PropertyInfo> properties)
+    {
+        StringBuilder.Clear();
         
-        var type = obj.GetType();
-        var properties = type.GetProperties();
+        foreach (var property in properties)
+        {
+            var ignoreAttribute = property.GetCustomAttribute<PsvIgnoreAttribute>();
+            if (ignoreAttribute is not null) continue;
+
+            var nameAttribute = property.GetCustomAttribute<PsvNameAttribute>();
+            if (nameAttribute is null)
+            {
+                StringBuilder.Append(property.Name);
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(nameAttribute.Name))
+                {
+                    throw new CustomAttributeFormatException($"{nameof(PsvNameAttribute)} указан с пустым значением {nameof(PsvNameAttribute.Name)}");
+                }
+                StringBuilder.Append(nameAttribute.Name);
+            }
+            StringBuilder.Append('|');
+        }
+
+        RemoveLastDelimiter();
+        return StringBuilder.ToString();
+    }
+    private static string GetPropertyValues(IEnumerable<PropertyInfo> properties, object obj)
+    {
+        StringBuilder.Clear();
+        
         foreach (var property in properties)
         {
             var attribute = property.GetCustomAttribute<PsvIgnoreAttribute>();
-            if (attribute is not null)
-            {
-                continue;
-            }
-
-            stringBuilder.Append(property.GetValue(obj));
-            stringBuilder.Append('|');
+            if (attribute is not null) continue;
+            
+            StringBuilder.Append(property.GetValue(obj));
+            StringBuilder.Append('|');
         }
 
-        stringBuilder.Remove(stringBuilder.Length - 1, 1);
-        return stringBuilder.ToString();
+        RemoveLastDelimiter();
+        return StringBuilder.ToString();
+    }
+    public static string Serialize(object obj)
+    {
+        var type = obj.GetType();
+        var properties = type.GetProperties();
+        return $"""
+                {GetPropertyNames(properties)}
+                {GetPropertyValues(properties, obj)}
+                """;
     }
 }

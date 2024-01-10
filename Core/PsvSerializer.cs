3c8339336cbd00bbe50682c47e8b279c947b0d9a
Starinin Andrey <starininandrey@gmail.com>
2024-01-10 20:04:24 +0300

    Add PsvSerializer.cs

---
+++ Core/PsvSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Text;
+using PSVSerializer.Attributes;
+
+namespace PSVSerializer.Core;
+
+public static class PsvSerializer
+{
+    public static string Serialize(object obj)
+    {
+        var stringBuilder = new StringBuilder();
+        
+        var type = obj.GetType();
+        var properties = type.GetProperties();
+        foreach (var property in properties)
+        {
+            var attribute = property.GetCustomAttribute<PsvIgnoreAttribute>();
+            if (attribute is not null)
+            {
+                continue;
+            }
+
+            stringBuilder.Append(property.GetValue(obj));
+            stringBuilder.Append('|');
+        }
+
+        stringBuilder.Remove(stringBuilder.Length - 1, 1);
+        return stringBuilder.ToString();
+    }
+}

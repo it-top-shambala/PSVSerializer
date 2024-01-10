196ef5207639db7cab03316ec1a31def6f4d915c
Starinin Andrey <starininandrey@gmail.com>
2024-01-10 20:45:22 +0300

    Add PsvNameAttribute

---
+++ Attributes/PsvNameAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace PSVSerializer.Attributes;
+
+public class PsvNameAttribute : Attribute
+{
+    public string Name { get; }
+
+    public PsvNameAttribute(string name)
+    {
+        Name = name;
+    }
+}
